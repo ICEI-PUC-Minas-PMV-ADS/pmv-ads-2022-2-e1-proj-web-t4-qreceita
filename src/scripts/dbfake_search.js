7346cb897db3fb4db80729f79370202da0441b29
OdairMarra <114453508+OdairMarra@users.noreply.github.com>
2022-12-02 11:37:07 -0300

    Update dbfake_search.js

--- src/scripts/dbfake_search.js
+++ src/scripts/dbfake_search.js
@@ -36,7 +36,7 @@ class SearchClass {
 
             clear_content(this.main_id)
 
-            const j = JSON.parse(localStorage.receitas);
+            const j = JSON.parse(localStorage.getItem('receitas'));
 
             main_content_search.classList.add(`show-result-recipe`)
             main_content_search.classList.add(`${input_user.value}`)
