c28f3baf5140914ae2d19825d53476c774f3fdf6
OdairMarra <114453508+OdairMarra@users.noreply.github.com>
2022-12-05 20:56:14 -0300

    Update receita-escolhida-btn-compart.js

--- src/scripts/receita-escolhida-btn-compart.js
+++ src/scripts/receita-escolhida-btn-compart.js
@@ -13,3 +13,20 @@ function acao(){
     modal.style.display = "none";
   
 }
+
+
+
+
+//para fazer o compartilhamento da receita por meio da URL
+var copyBtn = document.getElementById('copyBtn');
+
+copyBtn.addEventListener('click', function(event) {
+
+    /*Pega url*/
+    var url = window.location.href.toString();
+
+    /*seta para que a mesma possa ser compartilhada*/
+    window.prompt("Copie para compartilhar a url", url);
+
+
+});
